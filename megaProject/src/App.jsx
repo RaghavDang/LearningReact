7b92e0f154667c79b5af6c47c7da707f3d43205c
RaghavDang <raghavdang060202@gmail.com>
2025-11-18 23:08:57 +0530

    postcardcommit

--- megaProject/src/App.jsx
+++ megaProject/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import authService from './appwrite/auth'
 import { login, logout } from './store/authSlice'
 import { Outlet } from 'react-router-dom'
+import { Footer, Header } from './components'
 
 function App() {
   
@@ -29,7 +30,7 @@ function App() {
     <div className='w-full block'>
       <Header/>
       <main>
-      Todo:   {/* <Outlet/> */}
+      Todo:   <Outlet/>
       </main>
       <Footer/>
     </div>
